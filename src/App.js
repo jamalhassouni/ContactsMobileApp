886ae59e8fcf57b9cd9bf07c8b98688658c64a86
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-04 17:13:28 +0100

    Rename reducer folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import  {createStore} from 'redux';
 import  {Provider} from 'react-redux';
-import  reducer from './reducer';
+import  reducer from './reducers';
 
 import Routes from "./RootNavigator";
 
