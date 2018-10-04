67132ff1a6f11a70d83345429b2d7be6be6a5ebc
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-04 18:09:50 +0100

    Modify contact interface design

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, View ,FlatList } from 'react-native';
+import { Text, StyleSheet, View ,FlatList,TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-
-
 
 class Contacts  extends Component {
 
+   onContactSelected = (contact) =>{
+   console.log(contact);
+
+   }
    renderContact = ({item}) =>{
-       return  <Text>{item.name}</Text>;
+      return (
+        <View style={styles.contact}>
+            <TouchableOpacity onPress={this.onContactSelected.bind(this,item)}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.phone}>{item.phone}</Text>
+            </TouchableOpacity>
+        </View>
+
+      );
    }
     render() {
         return (
@@ -25,6 +35,20 @@ class Contacts  extends Component {
 const styles = StyleSheet.create({
  container:{
      flex: 1,
+ },
+ contact:{
+     flex: 1,
+     padding: 20,
+     borderColor: '#ddd',
+     borderBottomWidth: 1,
+     backgroundColor: '#fff',
+ },
+ name:{
+   fontSize:16 ,
+ },
+ phone:{
+ fontSize:14,
+ color:'#ddd'
  }
 });
 
