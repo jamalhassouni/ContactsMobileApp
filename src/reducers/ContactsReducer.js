9f727c1026b5c9ef5474c5d9cdb7394867c88d3a
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-06 13:28:37 +0100

    refactor:checking if permission read contacts granted or not

--- src/reducers/ContactsReducer.js
+++ src/reducers/ContactsReducer.js
@@ -1,18 +1,44 @@
 import Contacts from "react-native-contacts";
+import { PermissionsAndroid } from "react-native";
 let contactsList = [];
-Contacts.getAll((err, contacts) => {
-  if (err) throw err;
+async function requestContactsPermission() {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+      {
+        title: "App Contacts Permission",
+        message: " App needs access to your Contacts "
+      },
+      PermissionsAndroid.PERMISSIONS.READ_PROFILE,
+      {
+        title: "Permission to Read Profile",
+        message: "App needs permission to read profile"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the Contacts");
+      Contacts.getAll((err, contacts) => {
+        if (err) throw err;
 
-  // contacts returned
-  contacts.map(contact => {
-    const contactInfo = {
-      familyName: contact.familyName,
-      givenName: contact.givenName,
-      phoneNumbers: contact.phoneNumbers,
-      emailAddresses: contact.emailAddresses
-    };
-    contactsList.push(contactInfo);
-  });
-});
+        // contacts returned
+        contacts.map(contact => {
+          const contactInfo = {
+            familyName: contact.familyName,
+            givenName: contact.givenName,
+            middleName: contact.middleName,
+            phoneNumbers: contact.phoneNumbers,
+            emailAddresses: contact.emailAddresses
+          };
+          contactsList.push(contactInfo);
+        });
+      });
+    } else {
+      console.log("Contacts permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+}
+requestContactsPermission();
 
 export default () => contactsList;
