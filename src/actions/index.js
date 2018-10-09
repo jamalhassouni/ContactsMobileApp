5f5274550b1c41c43b74182bd6a597ebbba96ff2
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-09 13:29:32 +0100

    refactor: Create  request Contacts Permission

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import Contacts from "react-native-contacts";
+import { PermissionsAndroid, Platform } from "react-native";
+let contactsList = [];
 export const selectContact = contact => {
   return {
     type: "select_contact",
@@ -9,5 +12,41 @@ export const fetchContact = contacts => {
   return {
     type: "FETCHING",
     payload: contacts
+  };
+};
+
+async function requestContactsPermission() {
+  if (Platform.OS == "android") {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+        {
+          title: "Contacts",
+          message: "This app would like to view your contacts. "
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("You can use the Contacts");
+        Contacts.getAll((err, contacts) => {
+          if (err) throw err;
+
+          // contacts returned
+          return (contactsList = contacts);
+        });
+      } else {
+        console.log("Contacts permission denied");
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  }
+}
+
+export const getAllContacts = () => {
+  return dispatch => {
+    requestContactsPermission().then(() => {
+      console.log("Contacts", contactsList);
+      dispatch({ type: "getAll", payload: contactsList });
+    });
   };
 };
