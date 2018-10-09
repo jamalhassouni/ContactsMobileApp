0a511eb30cc0b61431c8a5638f60900d397c523e
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-09 14:21:49 +0100

    refactor:add FETCHING Action

--- src/reducers/ContactsReducer.js
+++ src/reducers/ContactsReducer.js
@@ -1,7 +1,9 @@
 const INITIAL_STATE = { contacts: [] };
 
-export default (state = INITIAL_STATE, action) => {
+export default (state = {}, action) => {
     switch (action.type) {
+      case 'FETCHING':
+        return { contacts: action.payload };
         case 'getAll':
         return { contacts: action.payload };
         default:
