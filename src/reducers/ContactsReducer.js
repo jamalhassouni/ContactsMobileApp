548e37f8b6a86083de6042ef1ea46a79538ed495
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-09 13:32:36 +0100

    refactor: Move requestContactsPermission to actions

--- src/reducers/ContactsReducer.js
+++ src/reducers/ContactsReducer.js
@@ -1,52 +1,9 @@
-import Contacts from "react-native-contacts";
-import { PermissionsAndroid } from "react-native";
-let contactsList = [];
-async function requestContactsPermission() {
-  try {
-    const granted = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-      {
-        title: "App Contacts Permission",
-        message: " App needs access to your Contacts "
-      },
-      PermissionsAndroid.PERMISSIONS.READ_PROFILE,
-      {
-        title: "Permission to Read Profile",
-        message: "App needs permission to read profile"
-      }
-    );
-    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      console.log("You can use the Contacts");
-      Contacts.getAll((err, contacts) => {
-        if (err) throw err;
-
-        // contacts returned
-        contacts.map(contact => {
-          const contactInfo = {
-            familyName: contact.familyName,
-            givenName: contact.givenName,
-            middleName: contact.middleName,
-            phoneNumbers: contact.phoneNumbers,
-            emailAddresses: contact.emailAddresses
-          };
-          contactsList.push(contactInfo);
-        });
-      });
-    } else {
-      console.log("Contacts permission denied");
-    }
-  } catch (err) {
-    console.warn(err);
-  }
-}
-requestContactsPermission();
-
-const INITIAL_STATE = { contacts: contactsList };
+const INITIAL_STATE = { contacts: [] };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'FETCHING':
-            return { ...state, contacts: action.payload };
+        case 'getAll':
+        return { contacts: action.payload };
         default:
             return state;
     }
