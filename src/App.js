192d27847437578b88e475379a9e725cefe9797a
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-09 13:28:22 +0100

    fix:Fix applymiddleware error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-import reducer from "./reducers";
+import ReduxThunk from 'redux-thunk';
+import reducers from "./reducers";
 
 import Routes from "./RootNavigator";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={createStore(reducer)}>
+      <Provider
+      store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}
+       >
         <Routes />
       </Provider>
     );
