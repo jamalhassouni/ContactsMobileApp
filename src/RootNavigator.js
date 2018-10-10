5b7ad1b9f47fe201e210cb66e19c595e9bb6a3ce
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-10 15:51:45 +0100

    refactor:Update Header and title

--- src/RootNavigator.js
+++ src/RootNavigator.js
@@ -1,13 +1,20 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation";
 import ContactsComponents from "./components/Contacts";
 import Details from "./components/Details";
-
+import { Header } from "react-native-elements";
+//navigation.getParam('label')
 const Routes = createStackNavigator({
   contacts: {
     screen: ContactsComponents,
-    navigationOptions: ({ navigation }) => ( {
-      title: "Contact",
-      headerRight: navigation.getParam('label'),
+    navigationOptions: ({ navigation }) => ({
+      header: (
+        <Header
+          leftComponent={{ text: "Contacts", style: { color: "#fff" } }}
+          centerComponent={{ text: navigation.getParam('label'), style: { color: "#fff" } }}
+          rightComponent={{ icon: "home", color: "#fff" }}
+        />
+      )
     })
   },
   details: {
