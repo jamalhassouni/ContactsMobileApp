2c87be5d687815cd0711ef164ba28bc3c8944e4c
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-10 15:53:44 +0100

    refactor:Create SearchContacts action

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,47 +12,21 @@ export const RefreshList = () => {
 };
 
 export const fetchContact = contacts => {
-  return {
-    type: "FETCHING",
-    payload: contacts
-  };
-};
-
-/*
-async function requestContactsPermission() {
-  if (Platform.OS == "android") {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
-        {
-          title: "Contacts",
-          message: "This app would like to view your contacts. "
-        }
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        console.log("You can use the Contacts");
-        Contacts.getAll((err, contacts) => {
-          if (err) throw err;
-
-          // contacts returned
-          return (contactsList = contacts);
-        });
-      } else {
-        console.log("Contacts permission denied");
-      }
-    } catch (err) {
-      console.warn(err);
-    }
-  }
-}
-
-export const getAllContacts = () => {
   return dispatch => {
-    requestContactsPermission().then(() => {
-      console.log("Contacts", contactsList);
-      dispatch({ type: 'refresh_list' });
-      dispatch({ type: "getAll", payload: contactsList });
-    });
+    dispatch({ type: "FETCHING", payload: contacts });
   };
 };
-*/
+export const SearchContacts = (searchContacts, query) => {
+  return dispatch => {
+    if (query !== "") {
+      dispatch({ type: "searching", payload: searchContacts, query: query });
+    } else {
+      console.log("done", searchContacts.length);
+      dispatch({
+        type: "searchDone",
+        payload: searchContacts,
+        count: searchContacts.length
+      });
+    }
+  };
+};
