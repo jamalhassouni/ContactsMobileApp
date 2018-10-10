fa5870bbcedbda1679d65d32085e8234663a601f
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-10 15:54:39 +0100

    refactor:Create SearchContacts action

--- src/reducers/ContactsReducer.js
+++ src/reducers/ContactsReducer.js
@@ -1,17 +1,39 @@
-const INITIAL_STATE = { count: null, refreshing: false, data: [] };
+const INITIAL_STATE = {
+  count: 0,
+  loading: true,
+  refreshing: false,
+  data: [],
+  fullData: [],
+  query: ""
+};
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case "FETCHING":
-      return { ...state, data: action.payload, count: action.payload.length };
-    case "getAll":
+      return {
+        ...state,
+        data: action.payload,
+        fullData: action.payload,
+        count: action.payload.length,
+        loading: false
+      };
+    case "refresh_list":
+      return { ...state, refreshing: true, query: "" };
+    case "searching":
       return {
         ...state,
         data: action.payload,
         count: action.payload.length,
-        refreshing: false
+        query: action.query,
+        loading: false
       };
-    case "refresh_list":
-      return { ...state, refreshing: true };
+    case "searchDone":
+      return {
+        ...state,
+        data: action.payload,
+        count: action.count,
+        loading: false
+      };
+
     default:
       return state;
   }
