2370cd4d1631521e568d3cf751a9e1833f9247e5
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-10 15:50:41 +0100

    refactor:redesign home page and create a search bar

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,23 +2,25 @@ import React, { Component } from "react";
 import { PermissionsAndroid, Platform } from "react-native";
 import Contacts from "react-native-contacts";
 import {
-  Text,
   StyleSheet,
   View,
   FlatList,
-  TouchableOpacity,
+  ActivityIndicator,
+  SafeAreaView,
   RefreshControl
 } from "react-native";
 import { connect } from "react-redux";
+import _ from "lodash";
 import * as action from "../actions";
+import { List, ListItem, SearchBar } from "react-native-elements";
 class ContactsComponents extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      refreshing: false
-    };
   }
   componentDidMount() {
+    this.props.navigation.setParams({
+      label: `${this.props.countList} Contacts`
+    });
     this._fetchData();
   }
 
@@ -32,13 +34,6 @@ class ContactsComponents extends Component {
           if (granted === PermissionsAndroid.RESULTS.GRANTED) {
             Contacts.getAll((err, contacts) => {
               this.props.fetchContact(contacts);
-              this.props.navigation.setParams({
-                label: (
-                  <Text style={{ marginRight: 10 }}>
-                    {this.props.countList} Contacts
-                  </Text>
-                )
-              });
             });
           } else {
             // Handle
@@ -51,51 +46,130 @@ class ContactsComponents extends Component {
   };
   _onRefresh = () => {
     this._fetchData();
-    // this.props.getAllContacts();
     this.props.navigation.setParams({
-      label: (
-        <Text style={{ marginRight: 10 }}>{this.props.countList} Contacts</Text>
-      )
+      label: `${this.props.countList} Contacts`
     });
-    console.log("count refresh", this.props.countList);
-    console.log("refreshing...");
   };
 
   onContactSelected = contact => {
     this.props.selectContact(contact);
     this.props.navigation.navigate("details");
   };
+  _contains = (user, query) => {
+    if (
+      user.givenName !== null &&
+      user.givenName !== undefined &&
+      user.givenName.toLowerCase().includes(query)
+    ) {
+      return true;
+    } else if (
+      user.familyName !== null &&
+      user.familyName !== undefined &&
+      user.familyName.toLowerCase().includes(query)
+    ) {
+      return true;
+    } else if (
+      user.middleName !== null &&
+      user.middleName !== undefined &&
+      user.middleName.toLowerCase().includes(query)
+    ) {
+      return true;
+    }
 
+    return false;
+  };
+  handleSearch = text => {
+    const formattedQuery = text.trim().toLowerCase();
+    const data = _.filter(this.props.fullData, user => {
+      return this._contains(user, formattedQuery);
+    });
+    this.props.navigation.setParams({
+      label: `${this.props.countList} Contacts`
+    });
+    this.props.SearchContacts(data, text);
+    console.log(this.props.countList);
+  };
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <SearchBar
+        onChangeText={this.handleSearch}
+        placeholder="Type Here..."
+        lightTheme
+        round
+      />
+    );
+  };
   renderContact = ({ item }) => {
     const middleName = item.middleName || "";
     const givenName = item.givenName || "";
     const familyName = item.familyName || "";
     const FullName = givenName + " " + middleName + " " + familyName;
     const phone = item.phoneNumbers[0].number;
+    const avatar = item.thumbnailPath || null;
     return (
-      <View style={styles.contact}>
+      /* <View style={styles.contact}>
         <TouchableOpacity onPress={this.onContactSelected.bind(this, item)}>
           <Text style={styles.name}>{FullName}</Text>
           <Text style={styles.phone}>{phone}</Text>
         </TouchableOpacity>
+      </View>*/
+      <ListItem
+        onPress={this.onContactSelected.bind(this, item)}
+        roundAvatar
+        title={FullName}
+        subtitle={phone}
+        avatar={{ uri: avatar }}
+        containerStyle={{ borderBottomWidth: 0 }}
+      />
+    );
+  };
+  renderFooter = () => {
+    if (!this.props.loading) return null;
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <ActivityIndicator color="#00cec9" size="large" />
       </View>
     );
   };
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.props.contacts}
-          renderItem={this.renderContact}
-          keyExtractor={(item, index) => index.toString()}
-          refreshControl={
-            <RefreshControl
-              refreshing={this.props.refreshing}
-              onRefresh={this._onRefresh}
-            />
-          }
-        />
-      </View>
+      <SafeAreaView>
+        <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+          <FlatList
+            data={this.props.contacts}
+            renderItem={this.renderContact}
+            keyExtractor={(item, index) => index.toString()}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.props.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
+            ItemSeparatorComponent={this.renderSeparator}
+            ListHeaderComponent={this.renderHeader}
+            ListEmptyComponent={this.renderFooter}
+          />
+        </List>
+      </SafeAreaView>
     );
   }
 }
@@ -121,11 +195,14 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = state => {
-  console.log(state);
+  console.log("state", state);
   return {
     contacts: state.contacts.data,
+    fullData: state.contacts.fullData,
     countList: state.contacts.count,
-    refreshing: state.contacts.refreshing
+    refreshing: state.contacts.refreshing,
+    query: state.contacts.query,
+    loading: state.contacts.loading
   };
 };
 
