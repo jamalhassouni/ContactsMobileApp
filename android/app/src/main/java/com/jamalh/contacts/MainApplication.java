68f39ecc3eb825416e4ed9b70c3a71752d6a9ebe
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-14 20:58:23 +0100

    chore:Install react-native-linear-gradient package

--- android/app/src/main/java/com/jamalh/contacts/MainApplication.java
+++ android/app/src/main/java/com/jamalh/contacts/MainApplication.java
@@ -3,6 +3,7 @@ package com.jamalh.contacts;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new ReactNativeContacts()
       );
