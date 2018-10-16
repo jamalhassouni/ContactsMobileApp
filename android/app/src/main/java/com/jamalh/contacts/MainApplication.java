3161a3458e3fe7e27a265d73498fe9075a76a022
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-16 20:58:39 +0100

    chore:Install react-native-immediate-phone-call package and linking them with the project

--- android/app/src/main/java/com/jamalh/contacts/MainApplication.java
+++ android/app/src/main/java/com/jamalh/contacts/MainApplication.java
@@ -3,6 +3,7 @@ package com.jamalh.contacts;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.wumke.RNImmediatePhoneCall.RNImmediatePhoneCallPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.rt2zz.reactnativecontacts.ReactNativeContacts;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNImmediatePhoneCallPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage(),
             new ReactNativeContacts()
