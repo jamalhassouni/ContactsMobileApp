a02eac62cd51cca0abe8d7f3ace72531ce37ed96
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-17 16:56:32 +0100

    refactor:Add Method Delete Contact by recordId fix Details styles

--- src/components/Details.js
+++ src/components/Details.js
@@ -8,10 +8,10 @@ import {
   Text,
   ScrollView,
   PermissionsAndroid,
-  Platform,
-  Animated,
+  Animated
 } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
+import Contacts from "react-native-contacts";
 import { connect } from "react-redux";
 import { FloatingMenu } from "./common";
 import { avatarLetter, uniqueNumber } from "./common/Helper";
@@ -31,7 +31,8 @@ class Details extends Component {
     gradient = sampleSize(allGradients, 1);
     colors = gradient[0].colors;
     this.state = {
-      modalY: new Animated.Value(-height)
+      modalY: new Animated.Value(-height),
+      bounceValue: new Animated.Value(height) //This is the initial position of the subview
     };
   }
   componentDidMount() {
@@ -40,33 +41,38 @@ class Details extends Component {
     });
   }
   callContact(phone) {
-    // for android only  Ask the user for permission to make calls
-    if (Platform.OS == "android") {
-      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CALL_PHONE, {
-        title: "Contacts",
-        message: "This app would like to view your contacts."
-      })
-        .then(granted => {
-          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-            //  Initiate immediate phone call
-            RNImmediatePhoneCall.immediatePhoneCall(phone);
-          } else {
-            // Handle
-            Alert.alert(
-              "Contacts",
-              "You will not be able to use the call feature if we do not have permission to do so"
-            );
-          }
-        })
-        .catch(err => {
-          console.log("PermissionsAndroid", err);
-        });
-    }
+    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CALL_PHONE).then(
+      granted => {
+        if (granted) {
+          //  Initiate immediate phone call
+          RNImmediatePhoneCall.immediatePhoneCall(phone);
+        }
+      }
+    );
   }
   textContact(phone) {
     const url = `sms:${phone}`;
     this.lanuchUrl(url);
   }
+  DeleteContact = () => {
+    this.closeModalBottom();
+    PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS
+    ).then(granted => {
+      if (granted) {
+        // delete a record using only it's recordID
+        Contacts.deleteContact(
+          { recordID: this.props.contact.recordID },
+          (err, recordId) => {
+            if (err) throw err;
+            // contact deleted
+            console.log("deleted ", recordId);
+            this.props.navigation.navigate("contacts");
+          }
+        );
+      }
+    });
+  };
   lanuchUrl(url) {
     Linking.canOpenURL(url).then(supported => {
       if (!supported) {
@@ -103,64 +109,133 @@ class Details extends Component {
       </View>
     );
   };
-  openModal() {
+  openModalTop() {
+    this.closeModalBottom();
     Animated.timing(this.state.modalY, {
       duration: 300,
       toValue: 0
     }).start();
   }
 
-  closeModal() {
+  closeModalTop() {
     Animated.timing(this.state.modalY, {
       duration: 300,
       toValue: -height
     }).start();
   }
-  renderDivider =  () =>{
-    return ( <View
-      style={{
-        height: 0.5,
-        width: "80%",
-        backgroundColor: "#CED0CE",
-        marginLeft: "5%"
-      }}
-    />);
+  renderDivider = () => {
+    return (
+      <View
+        style={{
+          height: 0.5,
+          width: "80%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "5%"
+        }}
+      />
+    );
   };
-  userConfirm = () =>{
-    Alert.alert(
-      'confirmation',
-      'Are you sure you want to delete '+FullName,
-      [
-        {text: 'Yes', onPress: () => console.log('Yes pressed')},
-        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-      ],
-      //Add cancelable: false property if we don’t want Alert closed when click on black drop background
-     // { cancelable: false }
-    )
-  };
+  openModalButtom() {
+    this.closeModalTop();
+    //This will animate the transalteY of the modal bottom between 0 & 100 depending on its current state
+    //100 comes from the style below, which is the height of the modal bottom.
+    Animated.spring(this.state.bounceValue, {
+      toValue: 0,
+      //velocity: 3,
+      //tension: 2,
+      duration: 300
+      //friction: 8
+    }).start();
+  }
+
+  closeModalBottom() {
+    Animated.spring(this.state.bounceValue, {
+      toValue: height,
+      //velocity: 3,
+      //tension: 2,
+      duration: 300
+      //friction: 8
+    }).start();
+  }
   render() {
+    // render top modal
     var ModalTop = (
       <Animated.View
         style={[
-          styles.modal,
+          styles.ModalTop,
           { transform: [{ translateY: this.state.modalY }] }
         ]}
       >
-        <List containerStyle={[styles.list,{backgroundColor:"transparent"}]}>
+        <List
+          containerStyle={[styles.list, { backgroundColor: "transparent" }]}
+        >
           <ListItem
-           onPress={this.closeModal.bind(this)}
-            rightIcon={<Icon onPress={this.closeModal.bind(this)} name="close" color="#ff7979" />}
+            underlayColor={"rgba(255,255,255,0)"}
+            onPress={this.closeModalTop.bind(this)}
+            rightIcon={
+              <Icon
+                underlayColor={"rgba(255,255,255,0)"}
+                onPress={this.closeModalTop.bind(this)}
+                name="close"
+                color="#ff7979"
+              />
+            }
             containerStyle={{ borderBottomWidth: 0 }}
           />
           {this.renderDivider()}
           <ListItem
-          hideChevron
-          onPress={this.userConfirm}
-            title='Delete Contact'
+            underlayColor={"rgba(255,255,255,0)"}
+            hideChevron
+            onPress={this.openModalButtom.bind(this)}
+            title="Delete Contact"
             containerStyle={{ borderBottomWidth: 0 }}
           />
           {this.renderDivider()}
         </List>
+      </Animated.View>
+    );
+
+    // render  modal bottom
+    var ModalBottom = (
+      <Animated.View
+        style={[
+          styles.ModalBottom,
+          { transform: [{ translateY: this.state.bounceValue }] }
+        ]}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 16,
+            fontWeight: "bold",
+            marginBottom: 5
+          }}
+        >
+          Delete this contact?
+        </Text>
+        <Text style={{ fontSize: 16, margin: 10 }}>
+          Delete "{FullName}
+          "?
+        </Text>
+        <View
+          style={{
+            justifyContent: "center",
+            flexDirection: "row",
+            paddingBottom: 5
+          }}
+        >
+          <Button
+            buttonStyle={styles.mdBottom}
+            titleStyle={{ color: "red" }}
+            onPress={this.DeleteContact}
+            title="Yes"
+          />
+          <Button
+            buttonStyle={styles.mdBottom}
+            onPress={this.closeModalBottom.bind(this)}
+            title="Cancel"
+          />
+        </View>
       </Animated.View>
     );
     return (
@@ -185,7 +260,7 @@ class Details extends Component {
             <View style={styles.headerRight}>
               <Icon
                 style={styles.iconOptions}
-                onPress={this.openModal.bind(this)}
+                onPress={this.openModalTop.bind(this)}
                 name={"more-vert"}
                 size={20}
                 underlayColor={"rgba(255,255,255,0)"}
@@ -232,6 +307,7 @@ class Details extends Component {
           onPress={() => console.log("yes!")}
         />
         {ModalTop}
+        {ModalBottom}
       </View>
     );
   }
@@ -316,7 +392,7 @@ const styles = StyleSheet.create({
     top: 0,
     right: 50
   },
-  modal: {
+  ModalTop: {
     height: 120,
     width: width,
     position: "absolute",
@@ -324,6 +400,24 @@ const styles = StyleSheet.create({
     left: 0,
     backgroundColor: "#dff9fb",
     justifyContent: "center"
+  },
+  ModalBottom: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#dff9fb",
+    height: 140,
+    zIndex: 2,
+    padding: 10
+  },
+  mdBottom: {
+    backgroundColor: "rgba(92, 99,216, 1)",
+    width: 100,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5
   }
 });
 
