0752847cc456027064fac2d5353541a718b1de58
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-27 19:53:20 +0100

    Tesing AsyncStorage Api to store data locally

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
-import { View, StyleSheet, FlatList, Switch } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  AsyncStorage,
+  Alert
+} from "react-native";
 import { connect } from "react-redux";
 import { Button, List, ListItem } from "react-native-elements";
+import { Input } from "./common";
 class Options extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      getValue: "",
+      textInputData: ""
+    };
   }
+  setValueLocally = () => {
+    AsyncStorage.setItem("Key_27", this.state.textInputData);
+
+    Alert.alert("Value Stored Successfully.");
+  };
+  getValueLocally = () => {
+    AsyncStorage.getItem("Key_27").then(value =>
+      this.setState({ getValue: value })
+    );
+  };
   componentDidMount() {
     this.props.navigation.setParams({
       user: `Options 1 `
@@ -32,13 +54,33 @@ class Options extends Component {
   render() {
     return (
       <View style={styles.mainConatinerStyle}>
-        <List containerStyle={styles.list}>
-          <FlatList
-            data={this.props.contacts}
-            renderItem={this.renderOption}
-            keyExtractor={(item, index) => index.toString()}
+        <List containerStyle={styles.list}>{this.renderOption()}</List>
+        <View style={styles.MainContainer}>
+          <Input
+            placeholder="Enter Some Text here"
+            onChangeText={data => this.setState({ textInputData: data })}
+            underlineColorAndroid="transparent"
+            style={styles.TextInputStyle}
           />
-        </List>
+
+          <TouchableOpacity
+            onPress={this.setValueLocally}
+            activeOpacity={0.7}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}> SAVE VALUE LOCALLY </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={this.getValueLocally}
+            activeOpacity={0.7}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}> GET VALUE LOCALLY SAVED </Text>
+          </TouchableOpacity>
+
+          <Text style={styles.text}> {this.state.getValue} </Text>
+        </View>
       </View>
     );
   }
@@ -52,6 +94,40 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     marginTop: 0
+  },
+  MainContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    margin: 10
+  },
+
+  TextInputStyle: {
+    textAlign: "center",
+    height: 40,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#028b53",
+    borderRadius: 10
+  },
+
+  button: {
+    width: "100%",
+    height: 40,
+    padding: 10,
+    backgroundColor: "#4CAF50",
+    borderRadius: 7,
+    marginTop: 10
+  },
+
+  buttonText: {
+    color: "#fff",
+    textAlign: "center"
+  },
+
+  text: {
+    fontSize: 20,
+    textAlign: "center"
   }
 });
 
