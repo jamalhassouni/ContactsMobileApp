a970b60d5c046fe36df3aeb2a98abb3bf774d426
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-10-28 16:03:52 +0000

    refactor:Designing options page and using AsyncStorage to store value locally

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,86 +1,125 @@
 import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  AsyncStorage,
-  Alert
-} from "react-native";
+import { View, StyleSheet, AsyncStorage } from "react-native";
 import { connect } from "react-redux";
-import { Button, List, ListItem } from "react-native-elements";
-import { Input } from "./common";
+import * as action from "../actions";
+import { List, ListItem } from "react-native-elements";
 class Options extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      getValue: "",
-      textInputData: ""
+      sortValue: false,
+      displayPhoto: true,
+      viewAsFamilyName: false
     };
   }
-  setValueLocally = () => {
-    AsyncStorage.setItem("Key_27", this.state.textInputData);
 
-    Alert.alert("Value Stored Successfully.");
-  };
-  getValueLocally = () => {
-    AsyncStorage.getItem("Key_27").then(value =>
-      this.setState({ getValue: value })
-    );
-  };
-  componentDidMount() {
-    this.props.navigation.setParams({
-      user: `Options 1 `
+  componentWillMount() {
+    // get data from Storage
+    AsyncStorage.getItem("sortValue").then(value => {
+      if (value == "yes") {
+        this.setState({ sortValue: true });
+      } else {
+        this.setState({ sortValue: false });
+      }
+    });
+    AsyncStorage.getItem("displayPhoto").then(value => {
+      if (value == "yes") {
+        this.setState({ displayPhoto: true });
+      } else {
+        this.setState({ displayPhoto: false });
+      }
+    });
+    AsyncStorage.getItem("viewAsFamilyName").then(value => {
+      if (value == "yes") {
+        this.setState({ viewAsFamilyName: true });
+      } else {
+        this.setState({ viewAsFamilyName: false });
+      }
     });
   }
-  renderOption = () => {
-    return (
-      <ListItem
-        onPress={() => console.log("cliked")}
-        switchButton
-        hideChevron
-        trackColor={"#00BCD4"}
-        title={"jamal"}
-        subtitle={"hassouni"}
-        switched={false}
-        onSwitch={value => {
-          console.log("switched", value);
-        }}
-        containerStyle={{ borderBottomWidth: 0 }}
-      />
-    );
-  };
 
+  /**
+   * handleSort
+   *  check value of switch  and  update sortValue storage and state
+   * @Param  {Boolean} value  (value of sort list by family name)
+   */
+  handleSort = value => {
+    this.setState({ sortValue: value });
+    if (value) {
+      AsyncStorage.setItem("sortValue", "yes");
+    } else {
+      AsyncStorage.setItem("sortValue", "no");
+    }
+  };
+ /**
+   * handlePhotos
+   *  check value of switch  and  update displayPhoto storage and state
+   * @Param  {Boolean} value  (value of Display photos and info)
+   */
+  handlePhotos = value => {
+    this.setState({ displayPhoto: value });
+    if (value) {
+      AsyncStorage.setItem("displayPhoto", "yes");
+    } else {
+      AsyncStorage.setItem("displayPhoto", "no");
+    }
+  };
+ /**
+   * handleViewAsFamilyName
+   *  check value of switch  and  update viewAsFamilyName storage and state
+   * @Param  {Boolean} value  (value of view contact as Family name first )
+   */
+  handleViewAsFamilyName = () => {
+    this.setState({ viewAsFamilyName: value });
+    if (value) {
+      AsyncStorage.setItem("viewAsFamilyName", "yes");
+    } else {
+      AsyncStorage.setItem("viewAsFamilyName", "no");
+    }
+  };
   render() {
     return (
       <View style={styles.mainConatinerStyle}>
-        <List containerStyle={styles.list}>{this.renderOption()}</List>
-        <View style={styles.MainContainer}>
-          <Input
-            placeholder="Enter Some Text here"
-            onChangeText={data => this.setState({ textInputData: data })}
-            underlineColorAndroid="transparent"
-            style={styles.TextInputStyle}
+        <List containerStyle={styles.list}>
+          <ListItem
+            onPress={() => console.log("cliked")}
+            switchButton
+            hideChevron
+            trackColor={"#00BCD4"}
+            title={"Sort list by"}
+            subtitle={"Family Name"}
+            switched={this.state.sortValue}
+            onSwitch={value => {
+              this.handleSort(value);
+            }}
+            containerStyle={{ borderBottomWidth: 0 }}
           />
-
-          <TouchableOpacity
-            onPress={this.setValueLocally}
-            activeOpacity={0.7}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}> SAVE VALUE LOCALLY </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={this.getValueLocally}
-            activeOpacity={0.7}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}> GET VALUE LOCALLY SAVED </Text>
-          </TouchableOpacity>
-
-          <Text style={styles.text}> {this.state.getValue} </Text>
-        </View>
+          <ListItem
+            onPress={() => console.log("cliked")}
+            switchButton
+            hideChevron
+            trackColor={"#00BCD4"}
+            title={"Display photos and info"}
+            switched={this.state.displayPhoto}
+            onSwitch={value => {
+              this.handlePhotos(value);
+            }}
+            containerStyle={{ borderBottomWidth: 0 }}
+          />
+          <ListItem
+            onPress={() => console.log("cliked")}
+            switchButton
+            hideChevron
+            trackColor={"#00BCD4"}
+            title={"View contact as"}
+            subtitle={"Family Name first"}
+            switched={this.state.viewAsFamilyName}
+            onSwitch={value => {
+              this.handleViewAsFamilyName(value);
+            }}
+            containerStyle={{ borderBottomWidth: 0 }}
+          />
+        </List>
       </View>
     );
   }
@@ -88,46 +127,13 @@ class Options extends Component {
 
 const styles = StyleSheet.create({
   mainConatinerStyle: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#fff"
   },
   list: {
     borderTopWidth: 0,
     borderBottomWidth: 0,
     marginTop: 0
-  },
-  MainContainer: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-    margin: 10
-  },
-
-  TextInputStyle: {
-    textAlign: "center",
-    height: 40,
-    width: "100%",
-    borderWidth: 1,
-    borderColor: "#028b53",
-    borderRadius: 10
-  },
-
-  button: {
-    width: "100%",
-    height: 40,
-    padding: 10,
-    backgroundColor: "#4CAF50",
-    borderRadius: 7,
-    marginTop: 10
-  },
-
-  buttonText: {
-    color: "#fff",
-    textAlign: "center"
-  },
-
-  text: {
-    fontSize: 20,
-    textAlign: "center"
   }
 });
 
@@ -136,4 +142,4 @@ const mapStateToProp = state => {
     contacts: state.contacts.data
   };
 };
-export default connect(mapStateToProp)(Options);
+export default connect(mapStateToProp,action)(Options);
