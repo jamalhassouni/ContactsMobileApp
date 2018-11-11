a5c692d0fb7552d201aeb7548228e4a4c3acda63
Jamal Hassouni <jamalhassouni13@gmail.com>
2018-11-11 18:30:06 +0100

    refactor:wrap up item inside Contact Component

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -3,7 +3,6 @@ import {
   PermissionsAndroid,
   Platform,
   View,
-  Linking,
   StyleSheet,
   ActivityIndicator,
   SafeAreaView,
@@ -17,20 +16,16 @@ import { connect } from "react-redux";
 import filter from "lodash/filter";
 import * as action from "../actions";
 import {
-  defaultColors,
-  sumChars,
   uniqueList,
-  avatarLetter,
   _contains,
   groupArrayByFirstChar,
   getSectionList,
   getSectionListSize
 } from "./common/Helper";
 import { FloatingMenu } from "./common";
-import { Icon, List, ListItem, SearchBar, Avatar } from "react-native-elements";
-import Swipeable from "./Swipeable";
+import { List, SearchBar } from "react-native-elements";
 import Group from "./Group";
-import RNImmediatePhoneCall from "react-native-immediate-phone-call";
+import Contact from "./Contact";
 import GroupSectionList from "./GroupSectionList";
 const { width, height } = Dimensions.get("window");
 const ITEM_HEIGHT = 56.34; // height of item
@@ -170,23 +165,11 @@ class ContactList extends PureComponent {
     this._fetchData();
   };
 
-  // this method to navigate to the details page with selected contact
-  onContactSelected = contact => {
-    this.props.selectContact(contact);
-    this.props.navigation.navigate("details");
-  };
-
   // this method to navigate to the AddContact page
   onClickAddContact = () => {
     this.props.navigation.navigate("AddContact");
   };
 
-  //enable or disable scroll on swipe
-  setScrollEnabled(enable) {
-    this.setState({
-      enable
-    });
-  }
   // this method to handle search
   handleSearch = text => {
     const formattedQuery = text.trim().toLowerCase();
@@ -195,37 +178,6 @@ class ContactList extends PureComponent {
     });
     this.props.SearchContacts(data, text);
   };
-
-  // this method for check if openUrl supported or not
-  lanuchUrl(url) {
-    Linking.canOpenURL(url).then(supported => {
-      if (!supported) {
-        // Show  end user Message
-        Alert.alert("App Not supported");
-      } else {
-        Linking.openURL(url);
-      }
-    });
-  }
-
-  // this method to  open default android messenger
-
-  textContact(phone) {
-    const url = `sms:${phone}`;
-    this.lanuchUrl(url);
-  }
-
-  // this method for call to  contact phone number
-  callContact(phone) {
-    PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.CALL_PHONE).then(
-      granted => {
-        if (granted) {
-          //  Initiate immediate phone call
-          RNImmediatePhoneCall.immediatePhoneCall(phone);
-        }
-      }
-    );
-  }
 
   onScrollEnd = e => {
     const layout = e.nativeEvent.contentOffset.y;
@@ -259,25 +211,6 @@ class ContactList extends PureComponent {
     ];
     return { length, offset: (length + separator) * index + header, index };
   }
-  // this method for render separator between ListItem
-  renderSeparator = key => {
-    if (this.state.displayPhoto) {
-      this.marginLeft = "14%";
-    } else {
-      this.marginLeft = "6%";
-    }
-    return (
-      <View
-        key={key}
-        style={{
-          height: 0.5,
-          width: "86%",
-          backgroundColor: "#CED0CE",
-          marginLeft: this.marginLeft
-        }}
-      />
-    );
-  };
 
   // this method for render header
   renderHeader = () => {
@@ -293,90 +226,7 @@ class ContactList extends PureComponent {
       />
     );
   };
-  renderInfo = (contact, FullName, phone, givenName, background) => {
-    if (this.state.displayPhoto) {
-      this.GroupColor = Colors.text;
-      return (
-        <ListItem
-          onPress={this.onContactSelected.bind(this, contact)}
-          roundAvatar
-          title={FullName}
-          subtitle={phone}
-          avatar={
-            <Avatar
-              size="small"
-              rounded
-              overlayContainerStyle={{ backgroundColor: background }}
-              title={avatarLetter(givenName)}
-              activeOpacity={0.7}
-            />
-          }
-          containerStyle={{ borderBottomWidth: 0 }}
-        />
-      );
-    } else {
-      this.GroupColor = Colors.Group;
-      return (
-        <ListItem
-          onPress={this.onContactSelected.bind(this, contact)}
-          title={FullName}
-          containerStyle={{ borderBottomWidth: 0 }}
-        />
-      );
-    }
-  };
-  renderContact = (contact, index, viewAs) => {
-    const middleName = contact.middleName || "";
-    const givenName = contact.givenName || "";
-    const familyName = contact.familyName || "";
-    let FullName;
-    if (viewAs == "familyName") {
-      FullName = middleName + " " + familyName + " " + givenName;
-    } else {
-      FullName = givenName + " " + middleName + " " + familyName;
-    }
 
-    //const avatar = item.thumbnailPath || "";
-    const phone = contact.phoneNumbers[0].number;
-    let i = sumChars(givenName) % defaultColors.length;
-    let background = defaultColors[i];
-    const leftContent = (
-      <View style={styles.leftSwipeItem}>
-        <Icon
-          name={"phone"}
-          type="font-awesome"
-          size={30}
-          underlayColor={"rgba(255,255,255,0)"}
-          color={"#ee5253"}
-        />
-      </View>
-    );
-    const rightContent = (
-      <View style={styles.rightSwipeItem}>
-        <Icon
-          name={"textsms"}
-          size={30}
-          underlayColor={"rgba(255,255,255,0)"}
-          color={Colors.Group}
-        />
-      </View>
-    );
-
-    return [
-      <Swipeable
-        key={index}
-        index={index}
-        leftContent={leftContent}
-        rightContent={rightContent}
-        onLeftActionRelease={this.callContact.bind(this, phone)}
-        onRightActionRelease={this.textContact.bind(this, phone)}
-        setScrollEnabled={enable => this.setScrollEnabled(enable)}
-      >
-        {this.renderInfo(contact, FullName, phone, givenName, background)}
-      </Swipeable>,
-      this.renderSeparator(`sepa-${index}`)
-    ];
-  };
   // this method for render Indicator
   renderIndicator = () => {
     if (!this.props.loading) return null;
@@ -397,34 +247,6 @@ class ContactList extends PureComponent {
     );
   };
 
-  /*_onViewableItemsChanged = (
-    info = {
-      viewableItems: {
-        key,
-        isViewable,
-        item: { columns },
-        index,
-        section
-      },
-      changed: {
-        key,
-        isViewable,
-        item: { columns },
-        index,
-        section
-      }
-    }
-  ) => {
-    let me = this;
-    if (!this.state.isClick && this.state.group !== info.viewableItems[0].section.group) {
-      me.setState({ isShow: true,isClick:false, group: info.viewableItems[0].section.group });
-      setTimeout(() => {
-        this.setState({ isShow: false });
-      }, 3000);
-    } else {
-      this.setState({ isShow: false });
-    }
-  };*/
   renderContent() {
     if (this.props.contacts.length > 0) {
       return (
@@ -450,9 +272,16 @@ class ContactList extends PureComponent {
               this.scroller = ref;
             }}
             getItemLayout={this.getItemLayout}
-            renderItem={({ item, index, section }) =>
-              this.renderContact(item, index, this.state.viewAs)
-            }
+            renderItem={({ item, index, section }) => (
+              <Contact
+                navigation={this.props.navigation}
+                item={item}
+                index={index}
+                viewAs={this.state.viewAs}
+                section={section}
+                displayPhoto={this.state.displayPhoto}
+              />
+            )}
             renderSectionHeader={({ section: { group } }) => (
               <Group
                 index={group}
@@ -510,44 +339,6 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     backgroundColor: "transparent"
-  },
-  leftSwipeItem: {
-    flex: 1,
-    alignItems: "flex-end",
-    justifyContent: "center",
-    paddingRight: 20,
-    backgroundColor: "#fff"
-  },
-  rightSwipeItem: {
-    alignItems: "flex-start",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center",
-    paddingLeft: 20
-  },
-  modelView: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1,
-    position: "absolute",
-    backgroundColor: "red",
-    right: width / 2 - 25,
-    top: height / 2,
-    bottom: height / 2,
-    left: width / 2 - 25
-  },
-  viewShow: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#666",
-    width: 80,
-    height: 80,
-    borderRadius: 3
-  },
-
-  textShow: {
-    fontSize: 50,
-    color: "#fff"
   }
 });
 const mapStateToProps = state => {
